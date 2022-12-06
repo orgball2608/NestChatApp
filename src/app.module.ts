dde401f6e6a6a24145b56aabff9f8c587d0307d3
20120557 <81631701+orgball2608@users.noreply.github.com>
2022-12-06 21:40:24 +0700

    delete some file in main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -25,7 +23,7 @@ import entities from './utils/typeorm';
       entities,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
